8a863287536b5c6a8d985761a2d0f88adb192c14
ODIN\odind <odindafna2006@rambler.ru>
2016-09-27 10:19:25 +0300

    Added another test

--- run.py
+++ run.py
@@ -3,6 +3,23 @@ sys.path.append('./src')
 sys.path.append('./data')
 
 
+def test0():
+
+    import numpy as np
+    import time
+
+    size = 500
+    a = np.random.randn(size, size)
+    b = np.random.randn(size, size)
+    z = np.random.randn(size, size)
+    start_time = time.time()
+
+    for i in xrange(1,1000):
+        [aa+bb for aa, bb in zip([a,a,a,a,a], [b,b,b,b,b])]
+
+    print len(z)
+    print time.time() - start_time
+
 def test():
 
     import numpy as np
@@ -28,7 +45,7 @@ def main():
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
     net = network.Network([784, 30, 10])
     net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
-    #test()
+    #test0()
 
 
 
